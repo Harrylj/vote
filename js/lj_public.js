ca0f7c113bc288885d70d1bb64f6242267ac55dc
Harrylj <18200166593@163.com>
2018-02-27 00:44:36 +0800

    2018-02-27-0:44

--- js/lj_public.js
+++ js/lj_public.js
@@ -1,32 +1,66 @@
-$(function(){
+$(function() {
+	if(!window.localStorage) {
+		alert("浏览器支持localstorage");
+		return false;
+	} else {
+		var storage = window.localStorage;
+		//写入a字段
+		storage["a"] = 'page2.html';
+		//写入b字段
+		//storage.b = 1;
+		//写入c字段
+		storage.setItem("c", 3);
+		console.log(typeof storage["a"]);
+		console.log(typeof storage["b"]);
+		console.log(typeof storage["c"]);
+		console.log(storage.a);
+	}
+
+	//点击链接跳转详情页
+	$('.click-nav').on('click', function() {
+		window.localStorage.b = $(this).attr('href')
+		window.location.href = "detial.html"
+		return false;
+
+	})
+
 	//改变联系方式
-	function changeInfo(){
+	function changeInfo() {
 		//微信名称
 		var wxName = "2018hello",
-		//手机号码
+			//手机号码
 			phoneNum = "18200166666";
 		$('.wx-name').html(wxName);
 		$('.phone-num').html(phoneNum);
 	}
-	
+
 	changeInfo();
-	
+
 	//改变排列顺序
-	function changeList(){
+	function changeList() {
 		var arrList = new Array();
-		$('.more-list').children().each(function(){
-			var abc = Math.floor(Math.random()*5);
-			var thisIndex = $(this).index();
-			//等于0则隐藏
-			if(thisIndex%abc){
-				$(this).hide()
-			}
-			console.log(thisIndex,abc,!(thisIndex%abc))
+		var div_father = $('.more-list');
+		//
+		div_father.children().each(function() {
+			arrList.push($(this))
 		})
 
+		//乱序函数
+		var random = function(array) {
+			return array.sort(function() {
+				return Math.random() > 0.5
+			});
+		};
+
+		//乱序
+		random(arrList);
+
+		for(var i = 0; i < arrList.length; i++) {
+			div_father.append(arrList[i])
+		}
+
 	}
-	
+
 	changeList();
-	
-	
+
 })
